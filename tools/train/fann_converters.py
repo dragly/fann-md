59af6b2e9f894e45c8baf9cb746fd536e4712498
Svenn-Arne Dragly <s@dragly.com>
2014-04-19 16:41:13 +0200

    Updated training process to use combo of normal + Cascade 2

--- tools/train/fann_converters.py
+++ tools/train/fann_converters.py
@@ -13,8 +13,12 @@ def rescale(value, value_min, value_max):
     #return value
     return (value - value_min) / (value_max - value_min) * 0.8 + 0.1
 
-def convert_two_particle_hdf5_to_fann(filenames, output_dir, n_max=inf):    
+def convert_two_particle_hdf5_to_fann(filenames, output_dir, n_max=inf, train_ratio=0.8):    
+    if train_ratio >= 1.0 or train_ratio <= 0.0:
+        raise Exception("train_ratio must be in range [0.0, 1.0]. Got " + str(train_ratio))
+    
     train_file = open(join(output_dir, "train.fann"), "w")
+    validate_file = open(join(output_dir, "validate.fann"), "w")
     test_file = open(join(output_dir, "test.fann"), "w")
     bounds_file = open(join(output_dir, "bounds.fann"), "w")
     
@@ -26,10 +30,8 @@ def convert_two_particle_hdf5_to_fann(filenames, output_dir, n_max=inf):
     energy_max = -inf
     r12_min = inf
     r12_max = -inf
-    r13_min = inf
-    r13_max = -inf
-    angle_min = inf
-    angle_max = -inf
+    
+    all_states = []
     
     for statesFile in filenames:
         f = h5py.File(statesFile, "r")
@@ -42,9 +44,9 @@ def convert_two_particle_hdf5_to_fann(filenames, output_dir, n_max=inf):
             atoms = states.get(stateName)
             energy_min = min(energy_min, atoms.attrs["energy"])
             energy_max = max(energy_max, atoms.attrs["energy"])
+            all_states.append([atoms.attrs["r12"], atoms.attrs["energy"]])
             
         f.close()
-        
     n_states_total = min(n_states_total, n_max)
     
     bounds_file.write("%d %d\n" % (n_parameters, n_outputs))
@@ -53,48 +55,41 @@ def convert_two_particle_hdf5_to_fann(filenames, output_dir, n_max=inf):
     
     bounds_file.close()
     
-    n_states_train = int(0.8 * n_states_total)
-    n_states_test = n_states_total - n_states_train
+    n_states_train = int(train_ratio * n_states_total)
+    n_states_validate = n_states_total - n_states_train
+    n_states_test = n_states_total
     
-    print "Using", n_states_train, " of the states for training and", n_states_test, "for testing"
+    print "Using", n_states_train, " of the states for training and", n_states_validate, "for validation"
+    print "All states are used for testing"
         
     train_file.write("%d %d %d\n\n" % (n_states_train, n_parameters, n_outputs))
+    validate_file.write("%d %d %d\n\n" % (n_states_validate, n_parameters, n_outputs))
     test_file.write("%d %d %d\n\n" % (n_states_test, n_parameters, n_outputs))
     
     state_counter = 0
     
-    for statesFile in filenames:
-        f = h5py.File(statesFile, "r")
-        atomsMeta = f.get("atomMeta")
-        states = f.get("/states")
-        stateNames = []
+    shuffle(all_states)
+    
+    for state in all_states:
+        if state_counter > n_max:
+            break
+        if state_counter < n_states_train:
+            target_file = train_file
+        else:
+            target_file = validate_file
+            
+        atoms = states.get(stateName)
         
-        for stateName in states:
-            stateNames.append(stateName)
+        r12 = rescale(state[0], r12_min, r12_max)
         
-        # Shuffle to get a random order
-        shuffle(stateNames)
+        energy = rescale(state[1], energy_min, energy_max)
         
-        for stateName in stateNames:
-            if state_counter > n_max:
-                break
-            if state_counter < n_states_train:
-                target_file = train_file
-            else:
-                target_file = test_file
-                
-            atoms = states.get(stateName)
-            
-            r12 = rescale(atoms.attrs["r12"], r12_min, r12_max)
-            
-            energy = rescale(atoms.attrs["energy"], energy_min, energy_max)
-            
-            target_file.write("%.10f\n\n" % (r12))
-            #target_file.write("%.10f %.10f %.10f %.10f\n\n" % (r12, r13, angle, r23))
-            target_file.write("%.10f\n\n" % (energy))
-            state_counter += 1
-            
-        f.close()
+        target_file.write("%.10f\n\n" % (r12))
+        target_file.write("%.10f\n\n" % (energy))
+        
+        test_file.write("%.10f\n\n" % (r12))
+        test_file.write("%.10f\n\n" % (energy))
+        state_counter += 1
         
     train_file.close()
     test_file.close()
--- tools/train/fann_train_two_particles.py
+++ tools/train/fann_train_two_particles.py
@@ -32,36 +32,84 @@ if len(states_files) == 1:
     states_files = glob(states_files[0])
 
 # Convert the files and move them to the build path
-convert_two_particle_hdf5_to_fann(states_files, output_dir, n_max=10000)
+convert_two_particle_hdf5_to_fann(states_files, output_dir, train_ratio=0.85)
 
-ann = libfann.neural_net()
-
-#ann.create_shortcut_array((2,10,1))
-#ann.set_cascade_weight_multiplier(0.001)
-ann.create_standard_array((2,10,10,1))
-ann.set_training_algorithm(libfann.TRAIN_RPROP)
-ann.set_activation_function_hidden(libfann.SIGMOID)
-ann.set_activation_function_output(libfann.SIGMOID)
-
+# Load data
 train_data = libfann.training_data()
+validate_data = libfann.training_data()
 test_data = libfann.training_data()
 
 train_data_filename = str(join(output_dir, "train.fann"))
+validate_data_filename = str(join(output_dir, "validate.fann"))
 test_data_filename = str(join(output_dir, "test.fann"))
-print "Loading data:\n", train_data_filename, "\n", test_data_filename
+
+print "Loading data:\n", train_data_filename, "\n", validate_data_filename, "\n", test_data_filename
+
 train_data.read_train_from_file(train_data_filename)
+validate_data.read_train_from_file(validate_data_filename)
 test_data.read_train_from_file(test_data_filename)
 
-best_result = inf
-#for i in range(5):
-    #ann.cascadetrain_on_data(train_data, 1, 1, 1e-5)
-ann.train_on_data(train_data, 20000, 1000, 0.00001)
+# Create and train networks
+networks = []
+for network_count in range(10):
+    ann = libfann.neural_net()
+    
+    ann.set_training_algorithm(libfann.TRAIN_RPROP)
+    
+    ann.create_shortcut_array((2,10,1))
+    ann.set_cascade_weight_multiplier(0.001)
+    #ann.create_standard_array((2,5,5,1))
+    ann.set_activation_function_hidden(libfann.SIGMOID_SYMMETRIC)
+    ann.set_activation_function_output(libfann.SIGMOID_SYMMETRIC)
+    
+    network_pre_filename = str(join(output_dir, "fann_network_pre_" + str(network_count) + ".net"))
+    best_result = inf
+    for i in range(20):
+        ann.train_on_data(train_data, 500, 500, 0.00001)
+        ann.reset_MSE()
+        validate_result = ann.test_data(validate_data)
+        print "Validation: Best:", best_result, ", current:", validate_result
+        if validate_result < best_result:
+            best_result = validate_result
+            
+            ann.save(network_pre_filename)
+        else:
+            print "Validation: Early stopping!"
+            break
+    
+    network_filename = str(join(output_dir, "fann_network_" + str(network_count) + ".net"))
+    networks.append(network_filename)
+    for i in range(10):
+        ann.cascadetrain_on_data(train_data, 1, 1, 1e-5)
+        ann.reset_MSE()
+        validate_result = ann.test_data(validate_data)
+        print "Validation: Best:", best_result, ", current:", validate_result
+        if validate_result < best_result:
+            best_result = validate_result
+            ann.save(network_filename)
+        else:
+            print "Validation: Early stopping!"
+            break
+    ann.destroy()
 
-network_filename = str(join(output_dir, "fann_network.net"))
-ann.save(network_filename)
+# Network comparison
+best_test_result = inf
+for network_filename in networks:
+    ann = libfann.neural_net()
+    ann.create_from_file(network_filename)
+    ann.reset_MSE()
+    test_result = ann.test_data(test_data)
+    print "Final results for network", network_filename, ":\n"
+    print test_result
+    if test_result < best_test_result:
+        print "Currently the best. Saving..."
+        best_test_result = test_result
+        network_filename_final = str(join(output_dir, "fann_network.net"))
+        ann.save(network_filename_final)
+    ann.destroy()
 
 train_data.destroy_train()
+validate_data.destroy_train()
 test_data.destroy_train()
-ann.destroy()
 
-print "Network saved to:\n", network_filename
+print "Network saved to:\n", network_filename_final
