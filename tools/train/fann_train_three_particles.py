bbeb5ef31744d394ed7fecbd12bc1e32336566bb
Svenn-Arne Dragly <s@dragly.com>
2014-05-12 17:57:16 +0200

    Changed output to ELLIOT after FANN Tool testing

--- tools/train/fann_train_three_particles.py
+++ tools/train/fann_train_three_particles.py
@@ -64,7 +64,7 @@ for network_count in range(10):
     ann.set_cascade_weight_multiplier(0.001)
     #ann.create_standard_array((2,5,5,1))
     ann.set_activation_function_hidden(libfann.GAUSSIAN)
-    ann.set_activation_function_output(libfann.LINEAR)
+    ann.set_activation_function_output(libfann.ELLIOT_SYMMETRIC)
     
     
     network_filename = str(join(output_dir, "fann_network_" + str(network_count) + ".net"))
